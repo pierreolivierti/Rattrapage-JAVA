ff0614b1eb61c49b4adac7220e141f4ae36ed732
Pierre-Olivier <pierreolivier.tilloy@viacesi.fr>
2017-07-10 16:53:35 +0200

    fix

--- Game/src/main/Game.java
+++ Game/src/main/Game.java
@@ -47,25 +47,18 @@ public class Game implements Runnable {
 	private void init() {
 		window = new Window(title, width, height);		// Create the window
 		window.getFrame().addKeyListener(keyManager); 	// Allow us to listen for new user keyboard input
-<<<<<<< HEAD
 		Assets.init();									// Creating and cropping our assets
 		camera = new Camera(this, 0,0);					// Creating the game camera
 		handler = new Handler(this);
 		gameState = new GameState(handler);				// Creating a gameState 
 		menuState = new MenuState(handler);				// Creating a menuState
 		State.setState(gameState);						// Setting the state as gameState
-=======
-		Assets.init();					// Creating and cropping our assets
-		gameState = new GameState(this);		// Creating a gameState 
-		menuState = new MenuState(this);		// Creating a menuState
-		State.setState(gameState);			// Setting the state as gameState
->>>>>>> origin/master
 	}
 	
 	
 	private void tick() {
 		keyManager.tick();
-		if(State.getState() != null)		// If we are in a known state
+		if(State.getState() != null)	// If we are in a known state
 			State.getState().tick();	// Calling tick method from current state
 	}
 	
@@ -76,43 +69,39 @@ public class Game implements Runnable {
 			window.getCanvas().createBufferStrategy(3); 	// If there is no buffer strategy, create one with 3 buffers
 			return;
 		}
-<<<<<<< HEAD
-		graphics = bs.getDrawGraphics();				// Get the graphics from our buffer strategy
-=======
-		graphics = bs.getDrawGraphics();			// Get the grahpics from our buffer strategy
->>>>>>> origin/master
-		graphics.clearRect(0, 0, width, height);		// Clear the screen
-		if(State.getState() != null) {				// If we are in a known state
-			State.getState().render(graphics);		// Creates graphics with current state render method
+		graphics = bs.getDrawGraphics();					// Get the graphics from our buffer strategy
+		graphics.clearRect(0, 0, width, height);			// Clear the screen
+		if(State.getState() != null) {						// If we are in a known state
+			State.getState().render(graphics);				// Creates graphics with current state render method
 		}
-		bs.show();						// Display all the graphics on the monitor
-		graphics.dispose();					// Mark the graphics variable as usable
+		bs.show();											// Display all the graphics on the monitor
+		graphics.dispose();									// Mark the graphics variable as usable
 	}
 	
 	// First method of the new thread
 	public void run() {
 		init();
-		int fps = 60;								// Number of time want to execute tick() and render() in 1 second
-		double timePerTick = 1000000000 / fps; 					// Maximum time (in nano seconds) we have to execute 1 tick() and 1 render() to execute it 60 time in 1 second
+		int fps = 60;												// Number of time want to execute tick() and render() in 1 second
+		double timePerTick = 1000000000 / fps; 						// Maximum time (in nano seconds) we have to execute 1 tick() and 1 render() to execute it 60 time in 1 second
 		double delta = 0;								
 		long now;										
-		long lastTime = System.nanoTime();					// Sets lastTime to the current time in nano seconds
+		long lastTime = System.nanoTime();							// Sets lastTime to the current time in nano seconds
 		long timer = 0;
 		int ticks = 0;
 		while(running) {
 			now = System.nanoTime();
-			delta += (now - lastTime) / timePerTick;			// Calculate the amount of time computer have until call tick() and render()
-			timer += now - lastTime;					// Adding amount of time since we last call our running loop 
+			delta += (now - lastTime) / timePerTick;				// Calculate the amount of time computer have until call tick() and render()
+			timer += now - lastTime;								// Adding amount of time since we last call our running loop 
 			lastTime = now;
 			
-			if(delta >= 1){							// If delta is OK, we can call tick() and render()
+			if(delta >= 1){											// If delta is OK, we can call tick() and render()
 				tick();
 				render();
 				ticks++;
 				delta--;
 			}
 			
-			if(timer >= 1000000000){					// Checking if timer is running for more than 1 second
+			if(timer >= 1000000000){								// Checking if timer is running for more than 1 second
 				System.out.println("Ticks and Frames: " + ticks);	// Printing number of ticks and frame executed in 1 second
 				ticks = 0;
 				timer = 0;
